86905b77f2b8febcbf07b2ace648e128e7b91830
Oleksii Krutilko <oleksii.krutilko@oracle.com>
2021-08-23 18:43:45 +0300

    Aspects refactoring

--- src/test/java/Aspects.java
+++ src/test/java/Aspects.java
@@ -1,3 +1,4 @@
+import io.qameta.allure.model.ExecutableItem;
 import io.qameta.allure.model.Status;
 import io.qameta.allure.model.StepResult;
 import io.qameta.allure.model.TestResult;
@@ -11,25 +12,22 @@ public class Aspects {
 
     @Around("execution(* io.qameta.allure.listener.LifecycleNotifier.beforeStepStop(..))")
     public Object setStatusAllureStep(ProceedingJoinPoint pjp) throws Throwable {
-        List<StepResult> stepResultList = ((StepResult) pjp.getArgs()[0]).getSteps();
-        for (StepResult stepResult : stepResultList) {
-            if (Status.FAILED.equals(stepResult.getStatus())) {
-                ((StepResult) pjp.getArgs()[0]).setStatus(Status.FAILED);
-            } else if (Status.BROKEN.equals(stepResult.getStatus())) {
-                ((StepResult) pjp.getArgs()[0]).setStatus(Status.BROKEN);
-            }
-        }
-        return pjp.proceed();
+        return extracted(pjp);
     }
 
     @Around("execution(* io.qameta.allure.listener.LifecycleNotifier.beforeTestStop(..))")
     public Object setStatusAllureTest(ProceedingJoinPoint pjp) throws Throwable {
-        List<StepResult> stepResultList = ((TestResult) pjp.getArgs()[0]).getSteps();
+        return extracted(pjp);
+    }
+
+    private Object extracted(ProceedingJoinPoint pjp) throws Throwable {
+        ExecutableItem executableItem = ((ExecutableItem) pjp.getArgs()[0]);
+        List<StepResult> stepResultList = executableItem.getSteps();
         for (StepResult stepResult : stepResultList) {
             if (Status.FAILED.equals(stepResult.getStatus())) {
-                ((TestResult) pjp.getArgs()[0]).setStatus(Status.FAILED);
+                executableItem.setStatus(Status.FAILED);
             } else if (Status.BROKEN.equals(stepResult.getStatus())) {
-                ((TestResult) pjp.getArgs()[0]).setStatus(Status.BROKEN);
+                executableItem.setStatus(Status.BROKEN);
             }
         }
         return pjp.proceed();
