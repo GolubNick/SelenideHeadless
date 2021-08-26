6e8721b5bd65b1a320f27b1564f0a568c326cc29
Mykola Holub <mykola.holub@oracle.com>
2021-08-26 15:28:03 +0300

    Added some logs

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -3,23 +3,33 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.testng.SoftAsserts;
 import org.testng.annotations.*;
 import static com.codeborne.selenide.AssertionMode.SOFT;
+import static com.codeborne.selenide.AssertionMode.STRICT;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 @Listeners(SoftAsserts.class)
 public class TestBase {
 
+    private final String ACCEPT_ALL = "//*[@class='call']";
+
     @BeforeClass
     public void init() {
-        if (System.getenv("assertionMode").equals("SOFT")) {
+        Object assertionMode = System.getenv("assertionMode");
+        if ((assertionMode != null) && assertionMode.equals("SOFT")) {
             Configuration.assertionMode = SOFT;
+            System.out.println("Assertion Mode is SOFT");
+        } else {
+            Configuration.assertionMode = STRICT;
+            System.out.println("Assertion Mode is HARD");
         }
         Configuration.headless = Boolean.parseBoolean(System.getenv("headless"));
+        System.out.println("Headless Mode is " + Boolean.parseBoolean(System.getenv("headless")));
     }
 
     @BeforeMethod
     public void before() {
         addListener("AllureCustomListener", new AllureCustomListener());
+        System.out.println("BaseUrl is " + System.getenv("baseUrl"));
         open(System.getenv("baseUrl"));
     }
 
