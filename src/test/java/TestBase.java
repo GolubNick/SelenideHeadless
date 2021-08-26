c7df0c9a267d19570cdf9f586a9111a34ccd2dd9
Mykola Holub <mykola.holub@oracle.com>
2021-08-26 10:26:14 +0300

    env.properties was changed to env variables

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -2,11 +2,6 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.testng.SoftAsserts;
 import org.testng.annotations.*;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import static com.codeborne.selenide.AssertionMode.SOFT;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
@@ -14,23 +9,18 @@ import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 @Listeners(SoftAsserts.class)
 public class TestBase {
 
-    private String baseUrl = null;
-
     @BeforeClass
     public void init() {
-        Properties props = loadPropertiesFile("env.properties");
-        baseUrl = props.getProperty("baseUrl");
-        boolean isHeadless = Boolean.parseBoolean(props.getProperty("headless"));
-        if (props.get("assertionMode").equals("SOFT")) {
+        if (System.getenv("assertionMode").equals("SOFT")) {
             Configuration.assertionMode = SOFT;
         }
-        Configuration.headless = isHeadless;
+        Configuration.headless = Boolean.parseBoolean(System.getenv("headless"));
     }
 
     @BeforeMethod
     public void before() {
         addListener("AllureCustomListener", new AllureCustomListener());
-        open(baseUrl);
+        open(System.getenv("baseUrl"));
     }
 
     @AfterMethod
@@ -41,17 +31,5 @@ public class TestBase {
     @AfterTest
     public void tearDownTest() {
         Selenide.closeWebDriver();
-    }
-
-    private Properties loadPropertiesFile(String filePath) {
-
-        Properties prop = new Properties();
-
-        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(filePath)) {
-            prop.load(resourceAsStream);
-        } catch (IOException e) {
-            System.err.println("Unable to load properties file : " + filePath);
-        }
-        return prop;
     }
 }
